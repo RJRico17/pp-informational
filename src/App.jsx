d0b62d73302029b7951c410665a7ec065120d815
rjfredr <fredrickalexander1727@gmail.com>
2025-10-02 14:34:11 -0700

    information

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-import Header from './Header.jsx'
+import Header from './Header.jsx';
+import Card from './components/card.jsx';
+import data from './assets/data.js'
 
 function App() {
-
   return (
     <>
       <Header />
+      {data.map((item, index) => (
+        <Card topics={item} key={index} />
+      ))}
     </>
   )
 }
---
+++ src/assets/data.js
@@ -0,0 +1,29 @@
+ const data = [
+  {
+    "Parks": [
+      "Stanley Park",
+      "Sea to Sky Gondola",
+      "Buchart Gardens (Victoria)"
+    ],
+    "City": [
+      "Scootering in city"
+    ],
+    "Food": [
+      "Japadog",
+      "Poutine",
+      "Clam Chowder",
+      "Osmow Shwarma"
+    ],
+    "Attractions": [
+      "Granville Island",
+      "Rogers Arena",
+      "Richmond Night Market"
+    ],
+    "Shopping": [
+      "Richmond Centre",
+      "Tsawwassen Mills"
+    ]
+  }
+]
+
+ export default data;
